516598759086201556afccc8a1467acfb856ed92
Kevin Scroggins <nitro404@gmail.com>
2022-04-23 14:42:49 -0400

    Update to the latest version of Hunter

--- CMake/Hunter/HunterInit.cmake
+++ CMake/Hunter/HunterInit.cmake
@@ -3,7 +3,7 @@ include_guard()
 include(${CORE_DIRECTORY}/CMake/Hunter/HunterGate.cmake)
 
 HunterGate(
-	URL "https://github.com/cpp-pm/hunter/archive/v0.24.0.tar.gz"
-	SHA1 "a3d7f4372b1dcd52faa6ff4a3bd5358e1d0e5efd"
+	URL "https://github.com/cpp-pm/hunter/archive/v0.24.1.tar.gz"
+	SHA1 "4942227a6e6f5e64414c55b97ef98609de199d18"
 	FILEPATH "${CORE_DIRECTORY}/CMake/Hunter/HunterConfig.cmake"
 )
