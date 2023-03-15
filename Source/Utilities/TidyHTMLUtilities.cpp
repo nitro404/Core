8da3d9eee2d471d08838722dd74c698edff591a2
Kevin Scroggins <nitro404@gmail.com>
2023-03-15 00:16:55 -0400

    Fix TidyHTML5 buffer handle leaking buffer instance

--- Source/Utilities/TidyHTMLUtilities.cpp
+++ Source/Utilities/TidyHTMLUtilities.cpp
@@ -24,6 +24,7 @@ TidyBufferHandle createTidyBufferHandle() {
 
 	return TidyBufferHandle(tidyBuffer, [](TidyBuffer * tidyBufferHandle) {
 		tidyBufFree(tidyBufferHandle);
+		delete tidyBufferHandle;
 	});
 }
 
