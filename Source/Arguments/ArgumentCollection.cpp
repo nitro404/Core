71327104484fdab995b728a10df72078fc1916a6
Kevin Scroggins <nitro404@gmail.com>
2023-09-11 21:44:11 -0400

    Update arg collection with new constructor/multi-arg function variations
    - Add last value argument accessor function to argument collection.
    - Add argument collection constructor variations that accept different argument collection types for in-place initialization including vector, array, map & multi-map.
    - Add new templated multi-argument name argument collection function variations.

--- Source/Arguments/ArgumentCollection.cpp
+++ Source/Arguments/ArgumentCollection.cpp
@@ -9,6 +9,27 @@ const ArgumentCollection::ArgumentCase ArgumentCollection::DEFAULT_CASE = Argume
 ArgumentCollection::ArgumentCollection(ArgumentCollection::ArgumentCase caseType)
 	: m_case(caseType) { }
 
+ArgumentCollection::ArgumentCollection(std::vector<std::pair<std::string, std::string>> arguments, ArgumentCase caseType)
+	: m_case(caseType) {
+	for(const std::pair<std::string, std::string> & argument : arguments) {
+		addArgument(argument.first, argument.second);
+	}
+}
+
+ArgumentCollection::ArgumentCollection(std::map<std::string, std::string> arguments, ArgumentCase caseType)
+	: m_case(caseType) {
+	for(std::map<std::string, std::string>::const_iterator i = arguments.begin(); i != arguments.end(); ++i) {
+		m_arguments.insert(std::pair<std::string, std::string>(i->first, i->second));
+	}
+}
+
+ArgumentCollection::ArgumentCollection(std::multimap<std::string, std::string> arguments, ArgumentCase caseType)
+	: m_case(caseType) {
+	for(std::multimap<std::string, std::string>::const_iterator i = arguments.begin(); i != arguments.end(); ++i) {
+		m_arguments.insert(std::pair<std::string, std::string>(i->first, i->second));
+	}
+}
+
 ArgumentCollection::ArgumentCollection(const ArgumentCollection & argumentCollection) {
 	m_case = argumentCollection.m_case;
 
@@ -57,52 +78,71 @@ size_t ArgumentCollection::numberOfArguments() const {
 }
 
 size_t ArgumentCollection::numberOfArguments(const std::string & name) const {
-	std::string formattedName(formatArgument(name));
+	std::string formattedArgumentName(formatArgumentName(name));
 
-	if(formattedName.empty()) {
+	if(formattedArgumentName.empty()) {
 		return 0;
 	}
 
-	return m_arguments.count(formattedName);
+	return m_arguments.count(formattedArgumentName);
 }
 
 bool ArgumentCollection::hasArgument(const std::string & name) const {
-	std::string formattedName(formatArgument(name));
+	std::string formattedArgumentName(formatArgumentName(name));
 
-	if(formattedName.empty()) {
+	if(formattedArgumentName.empty()) {
 		return false;
 	}
 
-	return m_arguments.find(formattedName) != m_arguments.end();
+	return m_arguments.find(formattedArgumentName) != m_arguments.end();
 }
 
 std::string ArgumentCollection::getFirstValue(const std::string & name) const {
-	std::string formattedName(formatArgument(name));
+	std::string formattedArgumentName(formatArgumentName(name));
 
-	if(formattedName.empty()) {
-		return std::string();
+	if(formattedArgumentName.empty()) {
+		return {};
 	}
 
-	std::multimap<std::string, std::string>::const_iterator argument = m_arguments.find(formattedName);
+	std::multimap<std::string, std::string>::const_iterator argumentIterator(m_arguments.find(formattedArgumentName));
 
-	if(argument == m_arguments.end()) {
-		return std::string();
+	if(argumentIterator == m_arguments.end()) {
+		return {};
 	}
 
-	return argument->second;
+	return argumentIterator->second;
+}
+
+std::string ArgumentCollection::getLastValue(const std::string & name) const {
+	std::string formattedArgumentName(formatArgumentName(name));
+
+	if(formattedArgumentName.empty()) {
+		return {};
+	}
+
+	std::pair<std::multimap<std::string, std::string>::const_iterator, std::multimap<std::string, std::string>::const_iterator> argumentRange(m_arguments.equal_range(formattedArgumentName));
+
+	if(argumentRange.first == argumentRange.second) {
+		return {};
+	}
+
+	std::multimap<std::string, std::string>::const_iterator lastArgumentIterator(argumentRange.second);
+	lastArgumentIterator--;
+
+	return lastArgumentIterator->second;
 }
 
 std::vector<std::string> ArgumentCollection::getValues(const std::string & name) const {
-	std::string formattedName(formatArgument(name));
+	std::string formattedArgumentName(formatArgumentName(name));
 
-	if(formattedName.empty()) {
+	if(formattedArgumentName.empty()) {
 		return {};
 	}
 
 	std::vector<std::string> values;
-	std::pair<std::multimap<std::string, std::string>::const_iterator, std::multimap<std::string, std::string>::const_iterator> nameRange = m_arguments.equal_range(formattedName);
+	std::pair<std::multimap<std::string, std::string>::const_iterator, std::multimap<std::string, std::string>::const_iterator> argumentRange(m_arguments.equal_range(formattedArgumentName));
 
-	for(std::multimap<std::string, std::string>::const_iterator i = nameRange.first; i != nameRange.second; ++i) {
+	for(std::multimap<std::string, std::string>::const_iterator i = argumentRange.first; i != argumentRange.second; ++i) {
 		values.emplace_back(i->second);
 	}
 
@@ -110,34 +150,34 @@ std::vector<std::string> ArgumentCollection::getValues(const std::string & name)
 }
 
 bool ArgumentCollection::addArgument(const std::string & name, const std::string & value) {
-	std::string formattedName(formatArgument(name));
+	std::string formattedArgumentName(formatArgumentName(name));
 
-	if(formattedName.empty()) {
+	if(formattedArgumentName.empty()) {
 		return false;
 	}
 
-	m_arguments.insert(std::pair<std::string, std::string>(formattedName, value));
+	m_arguments.insert(std::pair<std::string, std::string>(formattedArgumentName, value));
 
 	return true;
 }
 
 void ArgumentCollection::removeArgument(const std::string & name) {
-	std::string formattedName(formatArgument(name));
+	std::string formattedArgumentName(formatArgumentName(name));
 
-	if(formattedName.empty()) {
+	if(formattedArgumentName.empty()) {
 		return;
 	}
 
-	m_arguments.erase(formattedName);
+	m_arguments.erase(formattedArgumentName);
 }
 
 void ArgumentCollection::clear() {
 	m_arguments.clear();
 }
 
-std::string ArgumentCollection::formatArgument(const std::string & data) const {
+std::string ArgumentCollection::formatArgumentName(std::string_view data) const {
 	if(data.empty()) {
-		return data;
+		return {};
 	}
 
 	std::string formattedData(Utilities::trimString(data));
@@ -176,15 +216,15 @@ bool ArgumentCollection::operator == (const ArgumentCollection & argumentCollect
 	}
 
 	for(std::multimap<std::string, std::string>::const_iterator i = m_arguments.begin(); i != m_arguments.end(); ++i) {
-		std::multimap<std::string, std::string>::const_iterator argument = argumentCollection.m_arguments.find(i->first);
+		std::multimap<std::string, std::string>::const_iterator argumentIterator(argumentCollection.m_arguments.find(i->first));
 
-		if(argument == m_arguments.end()) {
+		if(argumentIterator == m_arguments.end()) {
 			return false;
 		}
 
 		std::string key(i->first);
 
-		if(i->second != argument->second) {
+		if(i->second != argumentIterator->second) {
 			return false;
 		}
 	}
--- Source/Arguments/ArgumentCollection.h
+++ Source/Arguments/ArgumentCollection.h
@@ -15,6 +15,11 @@ public:
 	};
 
 	ArgumentCollection(ArgumentCase caseType = DEFAULT_CASE);
+	ArgumentCollection(std::vector<std::pair<std::string, std::string>> arguments, ArgumentCase caseType = DEFAULT_CASE);
+	template <size_t N>
+	ArgumentCollection(std::array<std::pair<std::string, std::string>, N> arguments, ArgumentCase caseType = DEFAULT_CASE);
+	ArgumentCollection(std::map<std::string, std::string> arguments, ArgumentCase caseType = DEFAULT_CASE);
+	ArgumentCollection(std::multimap<std::string, std::string> arguments, ArgumentCase caseType = DEFAULT_CASE);
 	ArgumentCollection(ArgumentCollection && argumentCollection) noexcept;
 	ArgumentCollection(const ArgumentCollection & argumentCollection);
 	ArgumentCollection & operator = (ArgumentCollection && argumentCollection) noexcept;
@@ -26,11 +31,24 @@ public:
 
 	size_t numberOfArguments() const;
 	size_t numberOfArguments(const std::string & name) const;
+	template <typename ...Arguments, typename = typename std::enable_if<sizeof...(Arguments) >= 2>::type>
+	size_t numberOfArguments(Arguments &&... arguments) const;
 	bool hasArgument(const std::string & name) const;
+	template <typename ...Arguments, typename = typename std::enable_if<sizeof...(Arguments) >= 2>::type>
+	bool hasArgument(Arguments &&... arguments) const;
 	std::string getFirstValue(const std::string & name) const;
+	template <typename ...Arguments, typename = typename std::enable_if<sizeof...(Arguments) >= 2>::type>
+	std::string getFirstValue(Arguments &&... arguments) const;
+	std::string getLastValue(const std::string & name) const;
+	template <typename ...Arguments, typename = typename std::enable_if<sizeof...(Arguments) >= 2>::type>
+	std::string getLastValue(Arguments &&... arguments) const;
 	std::vector<std::string> getValues(const std::string & name) const;
+	template <typename ...Arguments, typename = typename std::enable_if<sizeof...(Arguments) >= 2>::type>
+	std::vector<std::string> getValues(Arguments &&... arguments) const;
 	bool addArgument(const std::string & name, const std::string & value);
 	void removeArgument(const std::string & name);
+	template <typename ...Arguments, typename = typename std::enable_if<sizeof...(Arguments) >= 2>::type>
+	void removeArgument(Arguments &&... arguments);
 	void clear();
 
 	std::string toString() const;
@@ -38,15 +56,140 @@ public:
 	bool operator == (const ArgumentCollection & argumentCollection) const;
 	bool operator != (const ArgumentCollection & argumentCollection) const;
 
-private:
-	std::string formatArgument(const std::string & data) const;
-
-public:
 	static const ArgumentCase DEFAULT_CASE;
 
 protected:
+	std::string formatArgumentName(std::string_view data) const;
+
 	ArgumentCase m_case;
 	std::multimap<std::string, std::string> m_arguments;
 };
+
+template <size_t N>
+ArgumentCollection::ArgumentCollection(std::array<std::pair<std::string, std::string>, N> arguments, ArgumentCase caseType)
+	: m_case(caseType) {
+	for(const std::pair<std::string, std::string> & argument : arguments) {
+		addArgument(argument.first, argument.second);
+	}
+}
+
+template <typename ...Arguments, typename>
+size_t ArgumentCollection::numberOfArguments(Arguments &&... arguments) const {
+	std::string_view unpackedArguments[sizeof...(arguments)] = {arguments...};
+	size_t totalArgumentCount = 0;
+
+	for(size_t i = 0; i < sizeof...(arguments); i++) {
+		std::string formattedArgumentName(formatArgumentName(unpackedArguments[i]));
+
+		if(formattedArgumentName.empty()) {
+			continue;
+		}
+
+		totalArgumentCount += m_arguments.count(formattedArgumentName);
+	}
+
+	return totalArgumentCount;
+}
+
+template <typename ...Arguments, typename>
+bool ArgumentCollection::hasArgument(Arguments &&... arguments) const {
+	std::string_view unpackedArguments[sizeof...(arguments)] = {arguments...};
+
+	for(size_t i = 0; i < sizeof...(arguments); i++) {
+		std::string formattedArgumentName(formatArgumentName(unpackedArguments[i]));
+
+		if(!formattedArgumentName.empty() && m_arguments.find(formattedArgumentName) != m_arguments.end()) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+template <typename ...Arguments, typename>
+std::string ArgumentCollection::getFirstValue(Arguments &&... arguments) const {
+	std::string_view unpackedArguments[sizeof...(arguments)] = {arguments...};
+
+	for(size_t i = 0; i < sizeof...(arguments); i++) {
+		std::string formattedArgumentName(formatArgumentName(unpackedArguments[i]));
+
+		if(formattedArgumentName.empty()) {
+			continue;
+		}
+
+		std::multimap<std::string, std::string>::const_iterator argumentIterator(m_arguments.find(formattedArgumentName));
+
+		if(argumentIterator == m_arguments.end()) {
+			continue;
+		}
+
+		return argumentIterator->second;
+	}
+
+	return {};
+}
+
+template <typename ...Arguments, typename>
+std::string ArgumentCollection::getLastValue(Arguments &&... arguments) const {
+	std::string_view unpackedArguments[sizeof...(arguments)] = {arguments...};
+
+	for(size_t i = 0; i < sizeof...(arguments); i++) {
+		std::string formattedArgumentName(formatArgumentName(unpackedArguments[i]));
+
+		if(formattedArgumentName.empty()) {
+			continue;
+		}
+
+		std::pair<std::multimap<std::string, std::string>::const_iterator, std::multimap<std::string, std::string>::const_iterator> argumentRange(m_arguments.equal_range(formattedArgumentName));
+
+		if(argumentRange.first == argumentRange.second) {
+			continue;
+		}
+
+		std::multimap<std::string, std::string>::const_iterator lastArgumentIterator(argumentRange.second);
+		lastArgumentIterator--;
+
+		return lastArgumentIterator->second;
+	}
+
+	return {};
+}
+
+template <typename ...Arguments, typename>
+std::vector<std::string> ArgumentCollection::getValues(Arguments &&... arguments) const {
+	std::string_view unpackedArguments[sizeof...(arguments)] = {arguments...};
+	std::vector<std::string> values;
+
+	for(size_t i = 0; i < sizeof...(arguments); i++) {
+		std::string formattedArgumentName(formatArgumentName(unpackedArguments[i]));
+
+		if(formattedArgumentName.empty()) {
+			continue;
+		}
+
+		std::pair<std::multimap<std::string, std::string>::const_iterator, std::multimap<std::string, std::string>::const_iterator> argumentRange(m_arguments.equal_range(formattedArgumentName));
+
+		for(std::multimap<std::string, std::string>::const_iterator i = argumentRange.first; i != argumentRange.second; ++i) {
+			values.emplace_back(i->second);
+		}
+	}
+
+	return values;
+}
+
+template <typename ...Arguments, typename>
+void ArgumentCollection::removeArgument(Arguments &&... arguments) {
+	std::string_view unpackedArguments[sizeof...(arguments)] = {arguments...};
+
+	for(size_t i = 0; i < sizeof...(arguments); i++) {
+		std::string formattedArgumentName(formatArgumentName(unpackedArguments[i]));
+
+		if(formattedArgumentName.empty()) {
+			continue;
+		}
+
+		m_arguments.erase(formattedArgumentName);
+	}
+}
 
 #endif // _ARGUMENT_COLLECTION_H_
