aada1f8b7f392cabe518d6f0fcf847600f90726b
Kevin Scroggins <nitro404@gmail.com>
2023-12-24 01:45:53 -0500

    Update ZLib C library from version 1.2.13 to 1.3.0

--- CMake/Hunter/HunterConfig.cmake
+++ CMake/Hunter/HunterConfig.cmake
@@ -125,9 +125,9 @@ hunter_config(tinyxml2
 )
 
 hunter_config(ZLIB
-	URL "https://github.com/Telefrag-Software/zlib/archive/refs/heads/hunter-1.2.13.1.zip"
-	VERSION "1.2.13.1"
-	SHA1 "1189eb8d6a0d8f5b2cd676f09661f4864cc85896"
+	URL "https://github.com/Telefrag-Software/zlib/archive/refs/heads/hunter-1.3.0.0.zip"
+	VERSION "1.3.0.0"
+	SHA1 "553c130c05f514e5db8b4ae623347e717674a4d1"
 )
 
 hunter_config(zstd
