c68ba1b29baabe7dd4670f60769bfd8c3ca1e845
Kevin Scroggins <nitro404@gmail.com>
2023-12-23 20:30:18 -0500

    Update Hunter package manager from version 0.24.10 to 0.25.3
    - Update Hunter Gate to latest version to suppress CMake deprecation warning messages.

--- CMake/Hunter/HunterInit.cmake
+++ CMake/Hunter/HunterInit.cmake
@@ -3,7 +3,7 @@ include_guard()
 include(${CORE_DIRECTORY}/CMake/Hunter/HunterGate.cmake)
 
 HunterGate(
-	URL "https://github.com/cpp-pm/hunter/archive/v0.24.10.tar.gz"
-	SHA1 "52997241a19026f428bd2c0c1782606bb456ba90"
+	URL "https://github.com/cpp-pm/hunter/archive/v0.25.3.tar.gz"
+	SHA1 "0dfbc2cb5c4cf7e83533733bdfd2125ff96680cb"
 	FILEPATH "${CORE_DIRECTORY}/CMake/Hunter/HunterConfig.cmake"
 )
