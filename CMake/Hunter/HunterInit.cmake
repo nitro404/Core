91173e3780fe71461b21a54b168e21ad097275e2
Kevin Scroggins <nitro404@gmail.com>
2025-10-11 19:34:08 -0400

    Update Hunter package manager from version 0.25.3 to 0.25.8

--- CMake/Hunter/HunterInit.cmake
+++ CMake/Hunter/HunterInit.cmake
@@ -3,7 +3,7 @@ include_guard()
 include(${CORE_DIRECTORY}/CMake/Hunter/HunterGate.cmake)
 
 HunterGate(
-	URL "https://github.com/cpp-pm/hunter/archive/v0.25.3.tar.gz"
-	SHA1 "0dfbc2cb5c4cf7e83533733bdfd2125ff96680cb"
+	URL "https://github.com/cpp-pm/hunter/archive/v0.25.8.tar.gz"
+	SHA1 "26c79d587883ec910bce168e25f6ac4595f97033"
 	FILEPATH "${CORE_DIRECTORY}/CMake/Hunter/HunterConfig.cmake"
 )
