69ab84dc3d0b4d92e6660bf8966662e3a852cb1b
Kevin Scroggins <nitro404@gmail.com>
2026-01-06 20:56:57 -0500

    Use custom Hunter fork to avoid ZLib issue

--- CMake/Hunter/HunterInit.cmake
+++ CMake/Hunter/HunterInit.cmake
@@ -3,7 +3,7 @@ include_guard()
 include(${CORE_DIRECTORY}/CMake/Hunter/HunterGate.cmake)
 
 HunterGate(
-	URL "https://github.com/cpp-pm/hunter/archive/v0.26.6.tar.gz"
-	SHA1 "e70c29f878f5d5f5cdf1b9ccd628fb872e8624a8"
+	URL "https://github.com/Telefrag-Software/hunter/archive/v0.26.6.1.tar.gz"
+	SHA1 "bbb82011e198711f75a108ccaf216e0ff849fbb5"
 	FILEPATH "${CORE_DIRECTORY}/CMake/Hunter/HunterConfig.cmake"
 )
