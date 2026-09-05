2f6a434470772c1b11a930f9275346e690eafbc0
Kevin Scroggins <nitro404@gmail.com>
2026-09-05 13:56:21 -0400

    Update Hunter from version 0.26.6.3 to 0.26.10.1

--- CMake/Hunter/HunterInit.cmake
+++ CMake/Hunter/HunterInit.cmake
@@ -3,7 +3,7 @@ include_guard()
 include(${CORE_DIRECTORY}/CMake/Hunter/HunterGate.cmake)
 
 HunterGate(
-	URL "https://github.com/Telefrag-Software/hunter/archive/v0.26.6.3.tar.gz"
-	SHA1 "40bdfd3a9e55ee01270d4bd85cdca7656fdaadfe"
+	URL "https://github.com/Telefrag-Software/hunter/archive/v0.26.10.1.tar.gz"
+	SHA1 "aa02af4d52e99b2bfb764325e52e1bbcd3dde9ac"
 	FILEPATH "${CORE_DIRECTORY}/CMake/Hunter/HunterConfig.cmake"
 )
